9f3b0c8f3049a384bb95a475441c969a4f8b1f78
Charles Brian Quinn <cbq@highgroove.com>
2012-09-10 17:36:31 -0400

    Initial Commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FlickrCommentGame::Application.config.secret_token = 'd0e0d3366a51e8ba7fb2b3cd3ea37ed5b2be4c765ebfc26ae8785adbe65218dd2953b01479e27ca9fffe59982e2e9beda15d626fe20f00962f070724f8d9dd5b'
